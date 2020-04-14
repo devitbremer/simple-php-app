8957f27fafdc630a272b5e3a36e0624634b77a48
Devit Bremer <devitbremer@gmail.com>
2020-04-14 09:21:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 $bg_color = 'DodgerBlue';
 $hostname = gethostname();
 
-#<?php echo($bg_color);?>
-
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
